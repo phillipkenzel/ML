2ad81daa3dc29511aeb07358262ae0d73825ff75
Phillip Kenzel <phillip.kenzel@student.unisg.ch>
2018-08-25 21:30:41 +0200

    Adjusted comments.

--- ML_model_templates/KNN_model_template_interactive.py
+++ ML_model_templates/KNN_model_template_interactive.py
@@ -13,10 +13,10 @@ target_variable = str(input("Please define the target variable of your data set:
 X = df.drop(str(target_variable), axis=1).values
 y = df[str(target_variable)].values
 
-# Asks the user to define test_size and random_state hyperparameters.
+# Asks the user to define the test_size and random_state hyperparameters.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=(float(input("Please define the size of the test dataset in percent (without using the % symbol): "))/100), random_state=int(input("Please define the random state for splitting the original dataset: ")), stratify=y)
 
-# Asks the user to define number of nearest neighbors.
+# Asks the user to define the number of nearest neighbors.
 knn = KNeighborsClassifier(n_neighbors=int(input("Please define the number of nearest neighbors: ")))
 
 knn.fit(X_train, y_train)
